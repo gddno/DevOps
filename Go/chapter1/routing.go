3d83c969cc6dd4916fa790d54f853d95df13a735
Zhuravlev Dmitrii <gddno@inbox.ru>
2023-05-17 08:08:00 +0300

    Write translate html on page

--- Go/chapter1/routing.go
+++ Go/chapter1/routing.go
@@ -1,32 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 )
+// Тип пользователь
+type User struct{
+	Name string
+	Last_name string
+	City string
+	Born string
+}
 
 // Обработчик главной страницы
-func home(w http.ResponseWriter, r *http.Request) {
+func home_page(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		log.Println("Пошли на несуществующую страницу")
 		return
 	}
-	w.Write([]byte("Привет это главная страница"))
+	tmpl, _ := template.ParseFiles("html_templates/home_page.html")
+	tmpl.Execute(w, nil)
+	// w.Write([]byte("Привет это главная страница"))
 	log.Println("Перешли на Home")
 }
 
 // Обработчик страницы Об
-func about(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ну а эта страница о нашем сайте"))
+func about_page(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Ну а эта страница о нашем сайте"))
+	var dima User
+    err := json.Unmarshal([]byte("/json_template/dima.json"), &dima)
+
+    if err != nil {
+        fmt.Println("JSON decode error!")
+        return
+    }
+	w.Write([]byte(dima))
 	log.Println("Вернулись на about")
 }
 
 func main() {
 	// Регистрируем обработчики
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/about", about)
+	mux.HandleFunc("/", home_page)
+	mux.HandleFunc("/about", about_page)
 
 	log.Print("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
