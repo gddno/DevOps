b1a92791bcb6a2ee596a4c7b8c9c0e6e5a3ee033
Zhuravlev Dmitrii <gddno@inbox.ru>
2023-05-19 01:11:14 +0300

    Writed select html templates and insert into html template

--- Go/chapter1/routing.go
+++ Go/chapter1/routing.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	// "debug/elf"
 	"encoding/json"
+	// "fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Тип пользователь
@@ -24,7 +27,6 @@ func root_page(w http.ResponseWriter, r *http.Request) {
 	}
 	tmpl, _ := template.ParseFiles("html_templates/home_page.html")
 	tmpl.Execute(w, nil)
-	// w.Write([]byte("Привет это главная страница"))
 	log.Println("Перешли на Home")
 }
 
@@ -36,13 +38,12 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	}
 	tmpl, _ := template.ParseFiles("html_templates/home_page.html")
 	tmpl.Execute(w, nil)
-	// w.Write([]byte("Привет это главная страница"))
 	log.Println("Перешли на Home")
 }
 
 // Обработчик страницы Об
 func about_page(w http.ResponseWriter, r *http.Request) {
-	// w.Write([]byte("Ну а эта страница о нашем сайте"))
+
 	user := User{
 		Name:      "Dima",
 		Last_name: "Zhuravlev",
@@ -52,21 +53,43 @@ func about_page(w http.ResponseWriter, r *http.Request) {
 
 	//Сериализуем структуру в строку
 	jsonString, _ := json.Marshal(&user)
-	// if err != nil {
-	// 	log.Fatalln("marshal ", err.Error())
-	// }
-	// w.Write([]byte(dima))
+
 	w.Write(jsonString)
 	log.Println("Вернулись на about")
 
-	// deserializeJson, err := User{}
-	// err = json.Unmarshal([]byte(jsonString), &deserializeJson)
-	// if err != nil {
-	// 	log.Fatalln("unmarshal ", err.Error())
-	// }
+}
 
-	// w.Write([]byte(deserializeJson))
+func showSomeId(w http.ResponseWriter, r *http.Request) {
+	id1, _ := strconv.Atoi(r.URL.Query().Get("id1"))
+	id2, _ := strconv.Atoi(r.URL.Query().Get("id2"))
 
+	type ViewData struct {
+		Id1 string
+		Id2 string
+	}
+
+	data := ViewData{
+		Id1: strconv.Itoa(id1),
+		Id2: strconv.Itoa(id2),
+	}
+
+	tmpl, _ := template.ParseFiles("html_templates/showSameId.html")
+	tmpl.Execute(w, data)
+
+}
+func selectPage(w http.ResponseWriter, r *http.Request) {
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+	// fmt.Fprintf(w, "hsdgfhd %s", page)
+	if page == 1 {
+		tmpl, _ := template.ParseFiles("html_templates/1.html")
+		tmpl.Execute(w, nil)
+	} else if page == 2 {
+		tmpl, _ := template.ParseFiles("html_templates/2.html")
+		tmpl.Execute(w, nil)
+	} else {
+		tmpl, _ := template.ParseFiles("html_templates/3.html")
+		tmpl.Execute(w, nil)
+	}
 }
 
 func main() {
@@ -75,6 +98,8 @@ func main() {
 	mux.HandleFunc("/", root_page)
 	mux.HandleFunc("/home", home_page)
 	mux.HandleFunc("/about", about_page)
+	mux.HandleFunc("/show", showSomeId)
+	mux.HandleFunc("/select", selectPage)
 
 	log.Print("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
