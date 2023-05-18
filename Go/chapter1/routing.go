dc32c42c7a9b62d7713f41f39ae2402358931cec
Zhuravlev Dmitrii <gddno@inbox.ru>
2023-05-18 08:21:21 +0300

    Write serealiztion json

--- Go/chapter1/routing.go
+++ Go/chapter1/routing.go
@@ -6,17 +6,30 @@ import (
 	"log"
 	"net/http"
 )
+
 // Тип пользователь
-type User struct{
-	Name string
-	Last_name string
-	City string
-	Born string
+type User struct {
+	Name      string `json:"name"`
+	Last_name string `json:"last_name"`
+	City      string `json:"city"`
+	Born      string `json:"born"`
 }
 
 // Обработчик главной страницы
-func home_page(w http.ResponseWriter, r *http.Request) {
+func root_page(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		log.Println("Пошли на несуществующую страницу")
+		return
+	}
+	tmpl, _ := template.ParseFiles("html_templates/home_page.html")
+	tmpl.Execute(w, nil)
+	// w.Write([]byte("Привет это главная страница"))
+	log.Println("Перешли на Home")
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/home" {
 		http.NotFound(w, r)
 		log.Println("Пошли на несуществующую страницу")
 		return
@@ -30,21 +43,37 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 // Обработчик страницы Об
 func about_page(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("Ну а эта страница о нашем сайте"))
-	var dima User
-    err := json.Unmarshal([]byte("/json_template/dima.json"), &dima)
+	user := User{
+		Name:      "Dima",
+		Last_name: "Zhuravlev",
+		City:      "Krasnodar",
+		Born:      "30-12-1996",
+	}
 
-    if err != nil {
-        fmt.Println("JSON decode error!")
-        return
-    }
-	w.Write([]byte(dima))
+	//Сериализуем структуру в строку
+	jsonString, _ := json.Marshal(&user)
+	// if err != nil {
+	// 	log.Fatalln("marshal ", err.Error())
+	// }
+	// w.Write([]byte(dima))
+	w.Write(jsonString)
 	log.Println("Вернулись на about")
+
+	// deserializeJson, err := User{}
+	// err = json.Unmarshal([]byte(jsonString), &deserializeJson)
+	// if err != nil {
+	// 	log.Fatalln("unmarshal ", err.Error())
+	// }
+
+	// w.Write([]byte(deserializeJson))
+
 }
 
 func main() {
 	// Регистрируем обработчики
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home_page)
+	mux.HandleFunc("/", root_page)
+	mux.HandleFunc("/home", home_page)
 	mux.HandleFunc("/about", about_page)
 
 	log.Print("Запуск веб-сервера на http://127.0.0.1:4000")
