ce58f7c2c27859dd98283d44b2c951a273fc1a72
Zhuravlev Dmitrii <gddno@inbox.ru>
2023-05-31 00:26:39 +0300

    Learning strings lesson11 by golangifu.com

---
+++ Go/chapter1/golangify/lesson11.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main(){
+	// peace := "peace"
+	// var a = "peace"
+	// var c string = "peace"
+	// var blank string
+	// fmt.Printf("%v, %v, %v, %v\n",peace, a, c, blank)
+	// fmt.Println("Hello \nand baey!!!")
+	// fmt.Println(`Hello 
+	// \n and 
+	// baey!!!`)
+	// fmt.Printf("%v is a %[1]T\n", "literal string")
+	// fmt.Printf("%v is a %[1]T\n", `raw string literal`)
+
+	// var pi rune = 960
+	// var omega rune = 969
+	// var alpha rune = 940
+	// var bang byte = 33
+
+	// fmt.Printf("%c %c %c %c\n", pi, omega, alpha, bang)
+
+// 	grade := 'A'
+// 	var c = 'A'
+// 	var b rune = 'A'
+
+// 	fmt.Printf("%v %v %v", grade, c, b)
+// 
+	star := '*'
+	fmt.Printf("%v\n", star)
+	smile := '?'
+	fmt.Printf("%v\n", smile)
+	acute := 'é'
+	fmt.Printf("%v\n", acute)
+
+}
